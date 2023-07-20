47d7eb27ae6524d1ddfcf156d224604c5b08d624
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-07-20 17:27:11 +0200

    Implement reparametrization test

--- vae_sandbox/tests/test_vae.py
+++ vae_sandbox/tests/test_vae.py
@@ -12,7 +12,9 @@ from vae_sandbox.core.vae import VAE
 
 @pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((5, 10, 10), 100)])
 def test_encoder(in_dim, latent_dim):
-    """Test the encoder of the VAE."""
+    """
+    Test the encoder of the VAE.
+    """
     vae = VAE(in_dim=in_dim, latent_dim=latent_dim)
     x = Tensor(1, *in_dim)
     # Eval mode to avoid batch norm related errors
@@ -20,3 +22,15 @@ def test_encoder(in_dim, latent_dim):
     mu, var = vae.encode(x)
     assert mu.shape == (1, latent_dim)
     assert var.shape == (1, latent_dim)
+
+
+@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((5, 10, 10), 100)])
+def test_reparametrization(in_dim, latent_dim):
+    """
+    Test the reparametrization part of the VAE.
+    """
+    vae = VAE(in_dim=in_dim, latent_dim=latent_dim)
+    mu = Tensor(1, latent_dim)
+    logvar = Tensor(1, latent_dim)
+    vector = vae.reparametrize(mu, logvar)
+    assert vector.shape == (1, latent_dim)
