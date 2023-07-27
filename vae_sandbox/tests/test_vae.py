3abe7a714d3a7f3c6ac2d1b9969f8e6e676f4ad2
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-07-27 22:36:54 +0200

    Implement loss function

--- vae_sandbox/tests/test_vae.py
+++ vae_sandbox/tests/test_vae.py
@@ -10,7 +10,7 @@ import pytest
 from vae_sandbox.core.vae import VAE
 
 
-@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 24, 24), 100)])
+@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 48, 48), 100)])
 def test_encoder(in_dim, latent_dim):
     """
     Test the encoder of the VAE.
@@ -24,7 +24,7 @@ def test_encoder(in_dim, latent_dim):
     assert var.shape == (1, latent_dim)
 
 
-@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 24, 24), 100)])
+@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 48, 48), 100)])
 def test_reparametrization(in_dim, latent_dim):
     """
     Test the reparametrization part of the VAE.
@@ -36,7 +36,7 @@ def test_reparametrization(in_dim, latent_dim):
     assert z.shape == (1, latent_dim)
 
 
-@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 24, 24), 100)])
+@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 48, 48), 100)])
 def test_decoder(in_dim, latent_dim):
     """
     Test the reparametrization part of the VAE.
@@ -45,3 +45,15 @@ def test_decoder(in_dim, latent_dim):
     z = Tensor(1, latent_dim)
     y = vae.decode(z)
     assert y.shape == (1, *in_dim)
+
+
+@pytest.mark.parametrize("in_dim,latent_dim", [((3, 32, 32), 10), ((3, 48, 48), 100)])
+def test_loss(in_dim, latent_dim):
+    """
+    Test the reparametrization part of the VAE.
+    """
+    vae = VAE(in_dim=in_dim, latent_dim=latent_dim)
+    x = Tensor(1, *in_dim)
+    out = vae.forward(x)
+    loss = vae.calculate_loss(*out)
+    assert loss.shape == ()
